2e6557cabe31f019350b0297a202bb69765ac0d1
mbganesh <crazyganesh179@gmail.com>
2020-04-21 15:06:54 +0530

    Initial commit

---
+++ app/src/main/java/com/example/hideactionbar/HideActionbar.java
@@ -0,0 +1,12 @@
+package com.example.hideactionbar;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HideActionbar extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hide_main);
+    }
+}
